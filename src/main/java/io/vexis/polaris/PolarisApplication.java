e3335898ac5667f625cda92355bd0b7ce200b7a4
vexis <arturgs.dev@gmail.com>
2026-05-20 15:31:44 -0300

    inital commit

---
+++ src/main/java/io/vexis/polaris/PolarisApplication.java
@@ -0,0 +1,17 @@
+package io.vexis.polaris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/")
+public class PolarisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PolarisApplication.class, args);
+    }
+
+}
