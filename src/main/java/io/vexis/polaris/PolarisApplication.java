2d8831fe81be9c4334ce8834ec7d3237caff6600
vexis <arturgs.dev@gmail.com>
2026-05-20 15:40:28 -0300

    feat(polaris): add root endpoint and update context path

--- src/main/java/io/vexis/polaris/PolarisApplication.java
+++ src/main/java/io/vexis/polaris/PolarisApplication.java
@@ -2,6 +2,7 @@ package io.vexis.polaris;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,11 @@ public class PolarisApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PolarisApplication.class, args);
+    }
+
+    @GetMapping
+    public String hello() {
+        return "Hello";
     }
 
 }
